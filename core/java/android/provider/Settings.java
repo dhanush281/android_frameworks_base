dd0b60eed22b4ae197427a8a54fa8c58fa72378c
dhanush281 <dhanushvozh345@gmail.com>
2024-11-25 11:14:19 +0000

    SystemUI: Bring back good ol' circle battery style Co-authored-by: althafvly <althafvly@gmail.com> Co-authored-by: Bruno Martins <bgcngm@gmail.com> Co-authored-by: Michael Bestas <mkbestas@lineageos.org> Change-Id: If9c55253c8e8786b4998632241a872104b7e7e8e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6303,6 +6303,26 @@ public final class Settings {
         public static final String CHARGING_CONTROL_START_TIME = "charging_control_start_time";
 
         /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 1: Display the battery as a circle
+         * 2: Display the battery as plain text
+         * default: 0
+         * 
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+        /**
+         * Status bar battery %
+         * 0: Hide the battery percentage
+         * 1: Display the battery percentage inside the icon
+         * 2: Display the battery percentage next to the icon
+         * 
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_battery_percent";
+
+        /**
          * Target time when battery is fully charged in CUSTOM mode.
          * The value is represented as seconds from midnight.
          * @hide
